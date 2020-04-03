8aab5f8d60cef1d31278f68d2a2cdbd55efbb4ba
darvela <darvela@gmail.com>
2020-04-02 23:11:34 -0500

    Added contact photo, added mail icon, added line separator, layout configuration

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,58 +1,109 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class Contact extends StatelessWidget{
-
+class Contact extends StatelessWidget {
   String name;
   String experience;
+  String pathImage;
+    int hexColor;
 
-  Contact(this.name, this.experience);
+  Contact(this.name, this.experience, this.pathImage, this.hexColor);
 
   @override
   Widget build(BuildContext context) {
 
-    final contactName = Container (
+    BoxDecoration myBoxDecoration() {
+      return BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: Color(0xFFe6e6e6),
+            width: 1.0,
+          ),
+        ),
+      );
+    }
+
+    final photo = Container(
+      margin: EdgeInsets.only(
+        left: 10.0,
+      ),
+      width: 70.0,
+      height: 70.0,
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image:
+              DecorationImage(fit: BoxFit.cover, image: AssetImage(pathImage))),
+    );
+
+    final contactName = Container(
       margin: EdgeInsets.only(
         left: 20.0,
+        bottom: 10.0
       ),
-      child: Text (
+      child: Text(
         name,
         textAlign: TextAlign.left,
         style: TextStyle(
             fontFamily: "Inria_Sans",
-            fontSize: 26.0,
+            fontSize: 23.0,
             fontWeight: FontWeight.bold,
-            color: Colors.black
-        ),
+            color: Colors.black),
       ),
     );
 
-    final contactExperience = Container (
-      margin: EdgeInsets.only(
-        left: 20.0,
-        bottom: 20.0
-      ),
-      child: Text (
-        experience,
+    final contactExperience = Container(
+      margin: EdgeInsets.only(left: 20.0, bottom: 20.0),
+      child: Text(
+        "Experience: " + experience,
         textAlign: TextAlign.left,
         style: TextStyle(
             fontFamily: "Inria_Sans",
-            fontSize: 25.0,
+            fontSize: 22.0,
             fontWeight: FontWeight.normal,
-            color: Colors.grey
-        ),
+            color: Colors.grey),
       ),
     );
 
-    final contactInfo = Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    final contactText = Container(
+      width: 230.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[contactName, contactExperience],
+      ),
+    );
+
+    final mailIcon = Container(
+      margin: EdgeInsets.only(
+        right: 3.0,
+      ),
+      child: Icon(
+        Icons.mail,
+        color: Color(hexColor)
+      ),
+    );
+
+    final lineSeparator = Container(
+      decoration: myBoxDecoration(),
+      margin: EdgeInsets.only(
+          bottom: 15.0,
+          top: 15.0
+      ),
+    );
+
+    final contactInfo = Container(
+      child: Row(
+        children: <Widget>[photo, contactText, mailIcon],
+      ),
+    );
+
+
+    final contact = Column(
       children: <Widget>[
-        contactName,
-        contactExperience
+        contactInfo,
+        lineSeparator
       ],
     );
 
-    return contactInfo;
+    return contact;
   }
-
 }
--- lib/contact_list.dart
+++ lib/contact_list.dart
@@ -6,17 +6,16 @@ class ContactList extends StatelessWidget{
   Widget build(BuildContext context) {
     final list = ListView(
       children: <Widget>[
-        Contact("Juan Diego", "1 año"),
-        Contact("Pedro", "7 años"),
-        Contact("Edna Rocio", "20 años"),
-        Contact("Diego", "3 años"),
-        Contact("Astrid", "4 años"),
-        Contact("Critsina", "16 años"),
-        Contact("Liliana", "9 años"),
-        Contact("Alberto", "2 años"),
-        Contact("Constanza", "2 años"),
-        Contact("Pato Donald", "1 años"),
-        Contact("Almost before we knew", "1 años")
+        Contact("Juan Diego", "1 year","assets/img/1diego.jpg",0xFFbdbdbd),
+        Contact("Pedro", "7 years","assets/img/2juan.jpg",0xFFff0000),
+        Contact("Edna Rocio", "20 years","assets/img/3astrid.jpg",0xFFff0000),
+        Contact("Diego", "3 years","assets/img/4cristina.jpg",0xFFff0000),
+        Contact("Astrid", "4 years","assets/img/5liliana.jpg",0xFFff0000),
+        Contact("Critsina", "16 years","assets/img/6alberto.jpg",0xFFff0000),
+        Contact("Liliana", "9 years","assets/img/7rocio.jpg",0xFFff0000),
+        Contact("Alberto", "2 years","assets/img/8constanza.jpg",0xFFff0000),
+        Contact("Constanza", "2 years","assets/img/9pedro.jpg",0xFFff0000),
+        Contact("Pato Donald", "1 years","assets/img/10tonto.jpeg",0xFFff0000)
       ],
     );
     return list;
